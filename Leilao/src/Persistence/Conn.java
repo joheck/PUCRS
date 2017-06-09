2814cca62a4f2aa27cbf756b7eb96aa3fe0fba18
JOO_F <JOO_F@192.168.0.5>
2017-06-09 15:04:56 -0300

    Update

---
+++ Leilao/src/Persistence/Conn.java
@@ -0,0 +1,30 @@
+package Persistence;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author João França
+ */
+public class Conn {    
+    private void CadastroDAOJavaDb() throws SQLException {
+        try {
+             Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
+        } catch (ClassNotFoundException ex) {
+        }
+    }    
+  
+    
+    private static Connection getConnection() throws SQLException {
+        //derbyDB sera o nome do diretorio criado localmente
+        return DriverManager.getConnection("jdbc:derby:derbyDB");
+    }
+    
+    public static void main(String[] args) throws SQLException {
+    	Conn c = new Conn();    	
+    	c.getConnection();    	
+    	String sql = "drop teste";	
+	}  
+}
