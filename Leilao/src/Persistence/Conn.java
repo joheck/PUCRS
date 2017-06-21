004695c3c04f493f098efe5c3bf32ab6b407c911
JOO_F <JOO_F@192.168.0.7>
2017-06-21 08:39:29 -0300

    - TABELAS CRIADAS NO BANCO EMBUTIDO - DDL DAS TABELAS PODEM SER VISTAS NA PASTA SCRIPT

--- Leilao/src/Persistence/Conn.java
+++ Leilao/src/Persistence/Conn.java
@@ -19,33 +19,13 @@ abstract class Conn {
 		}
 	}    
 
-
 	private static Connection getConnection() throws SQLException {
 		//derbyDB sera o nome do diretorio criado localmente
 		return DriverManager.getConnection("jdbc:derby:derbyDB");
-	}
-
-	
-	
-	
-	
-	
-	
-	
+	}	
+ 
 	public static void main(String[] args) throws SQLException {
-		Connection c = Conn.getConnection();    	
-		System.out.println(c); 
-		
-		String sql = "drop table funcionario";
-		PreparedStatement s = c.prepareStatement(sql);
-		
-		s.execute();
-		
-		//ResultSet rs = s.getResultSet();
-		
-	//	rs.next();
-	//	System.out.println(rs.getString("NOME"));
+			
+	}
 	
-		
-	}  
 }
